034ba4f77176ef5a0e78d6df3ce98d36708ebf0f
Hardoim, Gabriel - Dell Team <gabriel.hardoim@dellteam.com>
2023-02-17 17:13:30 -0300

    base structure

---
+++ sudoku.js
@@ -0,0 +1,77 @@
+var numberSelected = null
+var squareSelected = null
+
+var clues = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
+
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
+
+window.onload = function() {
+    const digits = document.getElementById("digits")
+    const board = document.getElementById("board")
+    
+    for (let r = 0; r < 9; r++) {
+        let number = document.createElement("div")
+
+        number.addEventListener("click", function() {
+            if (null != numberSelected) numberSelected.classList.remove("selected")
+
+            numberSelected = this
+            numberSelected.classList.add("selected")
+        })
+
+        number.classList.add("number")
+        number.innerText = r + 1
+        number.id = r + 1
+
+        digits.appendChild(number)
+
+        for (let c = 0; c < 9; c++) {
+            let square = document.createElement("div")
+            
+            square.addEventListener("click", function() {
+                if (numberSelected) {
+                    if ("" != this.innerText && !this.classList.contains("wrong")) return
+                    this.innerText = numberSelected.id
+                }
+
+                let coords = this.id.split("-")
+                let isRight = solution[parseInt(coords[0])][parseInt(coords[1])] == numberSelected.id
+
+                this.classList.remove(!isRight ? "right" : "wrong")
+                this.classList.add(isRight ? "right" : "wrong")
+            })
+
+            square.classList.add("square")
+            square.id = `${r}-${c}`
+
+            if ("-" != clues[r][c]) {
+                square.innerText = clues[r][c]
+                square.classList.add("start")
+            }
+
+            if (2 == r || 5 == r) square.classList.add("horizontal")
+            if (2 == c || 5 == c) square.classList.add("vertical")
+            board.appendChild(square)
+        }
+    }
+}
