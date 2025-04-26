11a511579397d5be86e19319ba2d96daf1b4a74d
Gabriel Hardoim <ghardoim@hotmail.com>
2025-04-26 01:14:55 -0300

    sudoku: combinations must have specific number

--- sudoku/helpers/helper.py
+++ sudoku/helpers/helper.py
@@ -9,11 +9,14 @@ class SudokuHelper:
         parser = ArgumentParser()
 
         parser.add_argument("--cannot-be", type=int, default=[0], nargs="+")
+        parser.add_argument("--must-has", type=int, default=[], nargs="+")
         parser.add_argument("--count", type=int)
         parser.add_argument("--sum", type=int)
-
         self.params = parser.parse_args()
 
         for possibles in combinations([i for i in range(1, 10) if i not in self.params.cannot_be], self.params.count):
-            if sum(possibles) == self.params.sum:
-                print(possibles)
+            if all([int(i) in possibles for i in self.params.must_has]):
+                if sum(possibles) == self.params.sum:
+                    print(possibles)
+
+if "__main__" == __name__: SudokuHelper()
